33d1287cfa163ca62b7e6cefd4461b0da5ab59f4
Ángel Raúl Recio Guerrero <redzero2930@gmail.com>
2023-03-13 16:43:53 -0600

    Primer commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+//!Antes que nada
+//?npm install firebase
+//?npm install uuid
+
+import './App.css';
+import {useState} from "react"
+import {storage} from './firebase'
+import {ref, uploadBytes} from 'firebase/storage'
+import {v4} from 'uuid'
+function App() {
+
+  const [imageUpload, setImageUpload] = useState(null)
+
+  const uploadImage = () => {
+    if(imageUpload == null) return;
+    const imageRef = ref(storage, `images/${imageUpload.name + v4()}`)
+    uploadBytes(imageRef, imageUpload).then(() => {
+      alert("Image Uploaded")
+    })
+  }
+
+  return (
+    <div className="App">
+      <input 
+      type="file" 
+      onChange={(event) => {
+        setImageUpload(event.target.files[0]);
+      }}
+    />
+      <button onClick={uploadImage}>Upload image</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+//!importar para poder acceder al almacenamiento de firebase
+import {getStorage} from 'firebase/storage'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDTftscnl2TYBV16z-eXgH36t0OV32Pj0k",
+  authDomain: "uploadingfile-2c53f.firebaseapp.com",
+  projectId: "uploadingfile-2c53f",
+  storageBucket: "uploadingfile-2c53f.appspot.com",
+  messagingSenderId: "789281848153",
+  appId: "1:789281848153:web:ecf098950b9147042baa48"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+//!Exportacion necesaria para usar el firebase
+export const storage = getStorage(app)
